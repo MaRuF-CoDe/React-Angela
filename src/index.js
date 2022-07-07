5d9a1e71eb8120cbef241062ebbfaa6f972273f1
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-07 16:23:02 +0600

    Inline Styling for React Elements

--- src/index.js
+++ src/index.js
@@ -1,36 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const img = "https://picsum.photos/200";
+const customStyle = {
+  color: "yellow",
+  fontSize: "30px",
+  border: "1px solid black"
+};
+
+customStyle.color = "green";
 
 ReactDom.render(
   <div>
-    <h1 className="heading" contentEditable="true" spellCheck="true">
-      My Favourite Foods
-    </h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Beef</li>
-      <li>WaterMelon</li>
-    </ul>
-    <div>
-      <img alt="random" src={img} />
-      <img
-        className="for-image"
-        alt="blue"
-        src="https://thumbs.dreamstime.com/b/water-splash-12429612.jpg"
-      />
-      <img
-        className="for-image"
-        alt="multi"
-        src="https://thumbs.dreamstime.com/b/colorful-powder-explosion-white-background-abstract-pastel-color-dust-particles-splash-152468326.jpg"
-      />
-      <img
-        className="for-image"
-        alt="light-blue"
-        src="https://t3.ftcdn.net/jpg/01/74/81/26/360_F_174812600_UjdOR4wCeQeXGG43RwlwfcPbKLD503pL.jpg"
-      />
-    </div>
+    <h1 style={{ color: "blue" }}> Hello World </h1>
+    <h1 style={customStyle}>Everyone</h1>
   </div>,
   document.getElementById("root")
 );

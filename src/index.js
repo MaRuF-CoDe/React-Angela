3100a61b62f33cce979ee710c944684f9a4cf425
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-07 17:51:55 +0600

    React Styling Practice

--- src/index.js
+++ src/index.js
@@ -1,18 +1,30 @@
 import React from "react";
 import ReactDom from "react-dom";
 
-const customStyle = {
-  color: "yellow",
-  fontSize: "30px",
-  border: "1px solid black"
+const today = new Date();
+const time = today.getHours();
+let day;
+
+const customColor = {
+  color: ""
 };
 
-customStyle.color = "green";
+if (time < 12) {
+  day = "Good Morning";
+  customColor.color = "red";
+} else if (time < 18) {
+  day = "Good Afternoon";
+  customColor.color = "green";
+} else {
+  day = "Good Night";
+  customColor.color = "blue";
+}
 
 ReactDom.render(
   <div>
-    <h1 style={{ color: "blue" }}> Hello World </h1>
-    <h1 style={customStyle}>Everyone</h1>
+    <h1 className="heading" style={customColor}>
+      {day}
+    </h1>
   </div>,
   document.getElementById("root")
 );

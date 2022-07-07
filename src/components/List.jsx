1540ca2904bc1ab968d9c440ffa526bcdec62784
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-07 21:34:44 +0600

    React Components

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function List() {
+  return (
+    <ul>
+      <li>Bacon</li>
+      <li>Beef</li>
+      <li>WaterMelon</li>
+    </ul>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,5 @@
 import React from "react";
 import ReactDom from "react-dom";
+import App from "./components/App";
 
-const today = new Date();
-const time = today.getHours();
-let day;
-
-const customColor = {
-  color: ""
-};
-
-if (time < 12) {
-  day = "Good Morning";
-  customColor.color = "red";
-} else if (time < 18) {
-  day = "Good Afternoon";
-  customColor.color = "green";
-} else {
-  day = "Good Night";
-  customColor.color = "blue";
-}
-
-ReactDom.render(
-  <div>
-    <h1 className="heading" style={customColor}>
-      {day}
-    </h1>
-  </div>,
-  document.getElementById("root")
-);
+ReactDom.render(<App />, document.getElementById("root"));

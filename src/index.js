6c89ce37f603f8e819942e244f61c565e2a991c5
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-13 16:57:32 +0600

    React Props Practice(Contact_list App)

--- src/index.js
+++ src/index.js
@@ -1,38 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import App from "./components/App";
 
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" />
-      <p>{props.num}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
-
-ReactDOM.render(
-  <div>
-    <h1>My Contacts</h1>
-    <Card
-      name="Beyonce"
-      img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      num="+123 456 789"
-      email="b@beyonce.com"
-    />
-    <Card
-      name="Jack Bauer"
-      img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      num="+987 654 321"
-      email="jack@nowhere.com"
-    />
-    <Card
-      name="Chuck Norris"
-      img="https://test-storage-mumbai.s3.amazonaws.com/uploads/image/file/11903/30_Beautiful_Indian_Girl.jpg"
-      num="+918 372 574"
-      email="gmail@chucknorris.com"
-    />
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));

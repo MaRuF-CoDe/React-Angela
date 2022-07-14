2a61e172991322d3ccc353da72b4bfcc7fa81500
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-15 00:00:02 +0600

    React DevTools

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Card from "./Card";
 import contacts from "../contacts";
+import Avatar from "./Avatar";
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="https://img.indiefolio.com/fit-in/1100x0/filters:format(webp):fill(transparent)/project/body/b8dd1d8464741368e1a6e1c648f885fa.jpg" />
       <Card
         name={contacts[0].name}
         img={contacts[0].imgURL}
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,17 @@
 import React from "react";
+import Avatar from "./Avatar";
+import Details from "./Details";
 
 function Card(props) {
   return (
     <div className="card">
       <div className="top">
         <h2 className="name">{props.name}</h2>
-        <img className="circle-img" src={props.img} alt="avatar_img" />
+        <Avatar img={props.img} />
       </div>
       <div className="bottom">
-        <p className="info">{props.phone}</p>
-        <p className="info">{props.email}</p>
+        <Details DetailInfo={props.email} />
+        <Details DetailInfo={props.phone} />
       </div>
     </div>
   );

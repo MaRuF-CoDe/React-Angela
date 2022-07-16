6969075987603a2fb39d7170f1adfdf7abb327cd
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-17 00:12:53 +0600

    Mapping Data to Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,29 +3,25 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+function CreatCard(contact) {
+  return (
+    <Card
+      key={contact.id}
+      id={contact.id}
+      name={contact.name}
+      img={contact.imgURL}
+      phone={contact.phone}
+      email={contact.email}
+    />
+  );
+}
+
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
       <Avatar img="https://img.indiefolio.com/fit-in/1100x0/filters:format(webp):fill(transparent)/project/body/b8dd1d8464741368e1a6e1c648f885fa.jpg" />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        phone={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        phone={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        phone={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(CreatCard)}
     </div>
   );
 }
--- src/contacts.js
+++ src/contacts.js
@@ -1,5 +1,6 @@
 const contacts = [
   {
+    id: 1,
     name: "Beyonce",
     imgURL:
       "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg",
@@ -7,6 +8,7 @@ const contacts = [
     email: "b@beyonce.com"
   },
   {
+    id: 2,
     name: "Jack Bauer",
     imgURL:
       "https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg",
@@ -14,6 +16,7 @@ const contacts = [
     email: "jack@nowhere.com"
   },
   {
+    id: 3,
     name: "Chuck Norris",
     imgURL:
       "https://test-storage-mumbai.s3.amazonaws.com/uploads/image/file/11903/30_Beautiful_Indian_Girl.jpg",

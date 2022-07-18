46dce216dc0f4bee96040c8bbf53ccc83424f75b
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-18 16:57:59 +0600

    Javascript ES6 Arrow functions

--- src/index.js
+++ src/index.js
@@ -8,63 +8,47 @@
 // const newNumbers = numbers.map(double);
 
 // var newNumbers = [];
-// numbers.forEach(function (x) {
-//   newNumbers.push(x * 2);
-// });
+// numbers.forEach((x)=>newNumbers.push(x * 2));
 
-// const newNumbers = numbers.map(function (x) {
-//   return x * 2;
-// });
+// const newNumbers = numbers.map(x=>x * 2);
 
 // console.log(newNumbers);
 
 //Filter - Create a new array by keeping the items that return true.
 
-// const newNumbers = numbers.filter(function(num) {
-//   return num < 10;
-// });
-
+// const newNumbers = numbers.filter(num=>num < 10);
 // var newNumbers = [];
-// numbers.forEach(function(num) {
+// numbers.forEach((num)=>{
 //   if (num < 10) {
 //     newNumbers.push(num);
 //   }
 // })
 
+// console.log(newNumbers)
+
 //Reduce - Accumulate a value by doing something to each item in an array.
 
-// var newNumber = numbers.reduce(function (accumulator, currentNumber) {
-//   console.log("accumulator = " + accumulator);
-//   console.log("currentNumber = " + currentNumber);
-//     return accumulator + currentNumber;
-// })
+// var newNumber = numbers.reduce((accumulator, currentNumber)=>accumulator + currentNumber)
 
 // var newNumber = 0;
-// numbers.forEach(function (currentNumber) {
-//   newNumber += currentNumber
-// })
+// numbers.forEach(currentNumber=>newNumber += currentNumber)
+
+// console.log(newNumber);
 
 //Find - find the first item that matches from an array.
 
-// const newNumber = numbers.find(function (num) {
-//   return num > 10;
-// })
-
+// const newNumber = numbers.find((num)=>num > 10)
 // console.log(newNumber);
 
 //FindIndex - find the index of the first item that matches.
 
-// const newNumber = numbers.findIndex(function (num) {
-//   return num > 10;
-// })
-
+// const newNumber = numbers.findIndex((num)=>num > 10)
 // console.log(newNumber);
 
 //Challenge Solution
 import emojipedia from "./emojipedia";
 
-const newEmojipedia = emojipedia.map(function (emojiEntry) {
-  return emojiEntry.meaning.substring(0, 100);
-});
-
+const newEmojipedia = emojipedia.map((emojiEntry) =>
+  emojiEntry.meaning.substring(0, 100)
+);
 console.log(newEmojipedia);

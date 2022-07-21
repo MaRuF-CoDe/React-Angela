8198642513b182674376223662544d0d82e4b08b
Md Maruf Bangabashi <maruf.170102@s.pust.ac.bd>
2022-07-21 15:13:03 +0600

    React Hooks - useState

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,20 @@
-import React from "react";
-import Form from "./Form";
-
-var userIsRegistered = false;
+import React, { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+
   return (
     <div className="container">
-      <Form isRegistered={userIsRegistered} />
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
     </div>
   );
 }
